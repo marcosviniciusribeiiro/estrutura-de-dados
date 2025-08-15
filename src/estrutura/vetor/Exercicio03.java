4b93fb636897dd7e4198970db43bfb289f9a3500
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-15 12:29:41 -0300

    Update Exercicio03.java

--- src/estrutura/vetor/Exercicio03.java
+++ src/estrutura/vetor/Exercicio03.java
@@ -1,10 +1,26 @@
 package estrutura.vetor;
 
+import java.util.Scanner;
+
 public class Exercicio03 {
+	public static Scanner sc;
+	
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		
+		Double[] notas = new Double[8];
+		double nota, soma = 0, media;
+		
+		for(int i = 0; i < notas.length; i++) {
+			System.out.println("Nota do " + (i+1) + "° Aluno: ");
+			nota = sc.nextDouble();
+			
+			notas[i] = nota;
+			soma += nota;
+		}
+		media = soma/(notas.length);
+		
+		System.out.println("Média da Turma: " + media);
+	}
 
 }
-/*
-3 - Crie um vetor para ler e armazenar as notas de 8 alunos. 
-Calcule e exiba a média das notas.
-
-*/
