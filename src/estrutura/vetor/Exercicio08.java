020ef06219b34ec8950a5de7205bbde637d8f6a4
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-15 18:47:49 -0300

    Update Exercicio08.java

--- src/estrutura/vetor/Exercicio08.java
+++ src/estrutura/vetor/Exercicio08.java
@@ -1,6 +1,7 @@
 package estrutura.vetor;
 
 import java.util.Random;
+import java.util.Arrays;
 
 public class Exercicio08 {
 	public static void main(String[] args) {
@@ -11,6 +12,35 @@ public class Exercicio08 {
 		for(int i = 0; i < numerosSorteados.length; i++) {
 			numerosSorteados[i] = aleatorio.nextInt(101);
 		}
+		
+		int[] tempUnicos = new int[20];
+		int contadorUnicos = 0;
+		
+		for(int i = 0 ; i < numerosSorteados.length; i++) {
+			int numeroAtual = numerosSorteados[i];
+			boolean jaExiste = false;
+			
+			for(int j = 0; j < numerosSorteados.length; j++) {
+				if(tempUnicos[j] == numeroAtual) {
+					jaExiste = true;
+					break;
+				}
+			}
+			
+			if(!jaExiste) {
+				tempUnicos[contadorUnicos] = numeroAtual;
+				contadorUnicos++;
+			}
+		}
+		
+		int[] numerosUnicos = new int[contadorUnicos];
+		for(int i = 0; i < contadorUnicos; i++) {
+			numerosUnicos[i] = tempUnicos[i]; 
+		}
+		
+		System.out.println("Números Sorteados: " + Arrays.toString(numerosSorteados));
+		System.out.println("Números Únicos: " + Arrays.toString(numerosUnicos));
+		
 	}
 }
 
