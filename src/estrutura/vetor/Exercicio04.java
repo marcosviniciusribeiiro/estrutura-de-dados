86ee918c1651c60a557c6a090bebc24ee33226ef
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-15 12:29:44 -0300

    Update Exercicio04.java

--- src/estrutura/vetor/Exercicio04.java
+++ src/estrutura/vetor/Exercicio04.java
@@ -1,15 +1,30 @@
 package estrutura.vetor;
 
+import java.util.Scanner;
+
 public class Exercicio04 {
-
+	public static Scanner sc;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		sc = new Scanner(System.in);
+		
+		int[] numeros = new int[15];
+		int numero, i, contador = 0;
+		
+		
+		for(i = 0; i < numeros.length; i++) {
+			System.out.println("Numero " + (i+1) + ": ");
+			numero = sc.nextInt();
+			numeros[i] = numero;
+			
+			if(numero % 2 == 0) {
+				contador ++;
+			}
+		}
+		System.out.println("Numeros Pares: " + contador);
+		for(i = 0; i < numeros.length; i++) {
+			if(numeros[i] % 2 == 0) {
+				System.out.println(numeros[i]);
+			}
+		}
 	}
-
 }
-/*
-4 - Elabore um programa que leia 15 números inteiros e armazene-os em um vetor. 
-Em seguida, conte e exiba quantos desses números são pares.
-
-*/
