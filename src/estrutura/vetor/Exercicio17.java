ecc023b36218de80d288fc7b1f3c95023d186cfd
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-15 15:22:35 -0300

    Update Exercicio17.java

--- src/estrutura/vetor/Exercicio17.java
+++ src/estrutura/vetor/Exercicio17.java
@@ -1,12 +1,27 @@
 package estrutura.vetor;
 
-public class Exercicio17 {
-	public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Exercicio17 {
+	public static Scanner sc;
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		
+		int[] numeros = new int[3];
+		
+		for(int i = 0; i < numeros.length; i++) {
+			System.out.println("Número " + (i+1));
+			int numero = sc.nextInt();
+			numeros[i] = numero;
+			
+			if(numeros[i] < 0) {
+				numeros[i] = 0;
+			}
+		}
+				
+		for(int i = 0; i < numeros.length; i++) {
+			System.out.println(numeros[i]);
+		}
+		
 	}
 }
-/*
-17 - Leia 15 números e armazene-os em um vetor. 
-Substitua todos os valores negativos por zero e exiba o vetor resultante.
-
-*/
