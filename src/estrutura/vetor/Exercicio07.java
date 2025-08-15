be749e7c784600dae2a938edaee521752559fd49
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-15 13:26:01 -0300

    Update Exercicio07.java

--- src/estrutura/vetor/Exercicio07.java
+++ src/estrutura/vetor/Exercicio07.java
@@ -1,7 +1,11 @@
 package estrutura.vetor;
+import java.util.Scanner;
 
 public class Exercicio07 {
-
+	public static Scanner sc;
+	public static void main(String[] args) {
+		
+	}
 }
 /*
 7 - Dado dois vetores de 5 elementos cada, 
