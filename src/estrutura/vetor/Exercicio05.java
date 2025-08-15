0e8028df6061b947d4ebcb26ae9ceacab745982d
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-15 13:25:32 -0300

    Update Exercicio05.java

--- src/estrutura/vetor/Exercicio05.java
+++ src/estrutura/vetor/Exercicio05.java
@@ -1,15 +1,20 @@
 package estrutura.vetor;
-
+import java.util.Scanner;
 public class Exercicio05 {
-
+	public static Scanner sc;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		sc = new Scanner(System.in);
+		
+		int[] num = new int[10];
+		
+		for(int i = 0; i < num.length; i++) {
+			System.out.println((i+1) + "° Número:");
+			int n = sc.nextInt();
+			num[i] = n;
+		}
+		
+		for(int i = 9; i >= 0; i--) {
+			System.out.println(num[i]);
+		}
 	}
-
 }
-/*
-5 - Leia 10 números e armazene-os em um vetor. 
-Mostre os elementos na ordem inversa à de inserção.
-
-*/
