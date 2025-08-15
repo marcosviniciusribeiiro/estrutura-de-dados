6adb567dca315f4be53c848055e26215438264f1
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-15 12:04:30 -0300

    Update Exercicio01.java

--- src/estrutura/vetor/Exercicio01.java
+++ src/estrutura/vetor/Exercicio01.java
@@ -1,11 +1,24 @@
 package estrutura.vetor;
 
+import java.util.Scanner;
+
 public class Exercicio01 {
+	private static Scanner sc;
+	
 	public static void main(String[] args) {
+		sc = new Scanner(System.in);
 		
+		int[] numeros = new int[10];
+		
+		for(int i = 0; i < numeros.length; i++) {
+			System.out.println((i+1) + "° número:");
+			int numero = sc.nextInt();
+			
+			numeros[i] = numero;
+		}
+		
+		for(int i = 0; i < numeros.length; i++) {
+			System.out.println(numeros[i]);
+		}
 	}
 }
-/*
-1 - Crie um vetor de 10 elementos inteiros. 
-Solicite ao usuário que insira os valores e, em seguida, imprima todos os elementos na ordem em que foram inseridos.
-*/
