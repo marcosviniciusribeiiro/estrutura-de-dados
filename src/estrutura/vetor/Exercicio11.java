c159167fe8aa937c13cf615c177ffd393c07de78
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-15 20:37:46 -0300

    Update Exercicio11.java

--- src/estrutura/vetor/Exercicio11.java
+++ src/estrutura/vetor/Exercicio11.java
@@ -1,13 +1,38 @@
 package estrutura.vetor;
 
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Exercicio11 {
+	public static Scanner sc;
 	public static void main(String[] args) {
+		Random aleatorio = new Random();
+		sc = new Scanner(System.in);
+		
+		int[] numerosSorteador = new int[30];
+		
+		for(int i = 0; i < numerosSorteador.length; i++) {
+			numerosSorteador[i] = aleatorio.nextInt(100) + 1;
+		}
+		
+		System.out.println("Digite um número:");
+		int numero = sc.nextInt();
+		
+		boolean num = false;
+		
+		for(int i = 0; i < numerosSorteador.length; i++) {
+			if(numerosSorteador[i] == numero) {
+				num = true;
+				System.out.println("Número na Posição " + i);
+				break;
+			}
+
+		}
+		if(num == false) {
+			System.out.println("Numero não encontrado.");				
+		}
+		System.out.println(Arrays.toString(numerosSorteador));
 		
 	}
 }
-/*
-11 - Sortear 30 numeros inteiros e armazene no vetor. 
-Solicite ao usuário que insira um número e verifique se o numero aparece no vetor, 
-caso apareça exiba a mensagem da posição que o numero foi encontrado, 
-caso o numero não exista no vetor exiba uma mensagem informando que o numero não existe no vetor.
-*/
