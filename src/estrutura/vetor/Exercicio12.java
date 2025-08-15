cc985b41821c902e1219f753635c6d035d1e83bc
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-15 20:37:48 -0300

    Update Exercicio12.java

--- src/estrutura/vetor/Exercicio12.java
+++ src/estrutura/vetor/Exercicio12.java
@@ -1,11 +1,50 @@
 package estrutura.vetor;
 
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Exercicio12 {
+	public static Scanner sc;
 	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		
+		int[] numeros = new int[10];
+		int cont=0, num;
+		
+		for(int i = 0; i < numeros.length; i++) {
+			System.out.println("Numero " + (i+1) + ":");
+			numeros[i] = sc.nextInt();
+		}
+		
+		// Selecionando um numero para tirar do vetor
+		System.out.println("Digite um número para removê-lo:");
+		num = sc.nextInt();
+		
+		for(int numero : numeros) {
+			if(numero == num) {
+				cont++;
+			}
+		}
+		
+		int[] novoVetor = new int[numeros.length - cont];
+		
+		
+		if(cont>0) {
+			int j = 0;
+			for(int i = 0; i < numeros.length ; i++) {
+				if(numeros[i] != num) {
+					novoVetor[j] = numeros[i];
+					j++;
+				}
+			}
+			System.out.println("Novo vetor : " + Arrays.toString(novoVetor));
+		} else {
+			System.out.println("Número não encontrado. O array permanece o mesmo.");
+			System.out.println("Vetor original : " + Arrays.toString(numeros));
+		}
+		
+		
+		
 		
 	}
 }
-/*
-12 - Leia 10 números e armazene-os em um vetor. 
-Solicite ao usuário que insira um valor a ser removido. Crie um novo vetor sem esse valor, caso ele exista.
-*/
