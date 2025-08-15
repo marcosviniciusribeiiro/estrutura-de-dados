4ae5cf7ce70246f8e88a60798bb6048cc244e646
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-15 18:48:24 -0300

    Update Exercicio10.java

--- src/estrutura/vetor/Exercicio10.java
+++ src/estrutura/vetor/Exercicio10.java
@@ -1,12 +1,39 @@
 package estrutura.vetor;
 
+import java.util.Random;
+import java.util.Arrays;
+
 public class Exercicio10 {
 	public static void main(String[] args) {
+		Random aleatorio = new Random();
+		int[] numSorteado = new int[15];
+		int[] numImpar =  new int[15];
+		int[] numPar =  new int[15];
 		
+		int idxPar = 0, idxImpar = 0;
+		
+		for(int i = 0; i < numSorteado.length; i++) {
+			numSorteado[i] = aleatorio.nextInt(101);
+			int numAtual = numSorteado[i];
+			
+			if(numAtual % 2 == 0) {
+				numPar[idxPar] = numAtual;
+				idxPar++;
+			}else {
+				numImpar[idxImpar] = numAtual;
+				idxImpar++;
+			}
+		}
+		System.out.println("Números Sorteados: " + Arrays.toString(numSorteado));
+		
+		System.out.print("Números Par: ");
+		for(int i = 0; i < idxPar; i++) {
+			System.out.print(numPar[i] + " ");
+		}
+		
+		System.out.print("\nNúmeros Impar: ");
+		for(int i = 0; i < idxImpar; i++) {
+			System.out.print(numImpar[i] + " ");
+		}
 	}
 }
-/*
-10 - Sortear 15 números e armazene-os em um vetor.
- Os numero sorteados deverão está no intervalo de 1 até 100 e separe os números pares 
-e ímpares em dois novos vetores e exiba-os 3 vetores no final.
-*/
