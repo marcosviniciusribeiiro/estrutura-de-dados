8b890b1a2406dc2fc1e55f87d80fc6fb635b4793
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-16 08:41:50 -0300

    Update Exercicio14.java

--- src/estrutura/vetor/Exercicio14.java
+++ src/estrutura/vetor/Exercicio14.java
@@ -22,3 +22,10 @@ public class Exercicio14 {
 		System.out.println("Vetor com elementos invertidos: " + Arrays.toString(numeros));
 	}
 }
+
+/*14 - Sortear 10 números e armazene-os em um vetor. E inverter o vetor de forma que troque os elementos da:
+   posição 0 com a posição 9
+   posição 1 com a posição 8
+   posição 2 com a posição 7
+   posição 3 com a posição 6
+   posição 4 com a posição 5*/
