142fd25803648d2dfba6d66057eb505bc978b344
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-15 23:00:26 -0300

    Update Exercicio07.java

--- src/estrutura/vetor/Exercicio07.java
+++ src/estrutura/vetor/Exercicio07.java
@@ -7,7 +7,7 @@ public class Exercicio07 {
 		int[] v1 = {1, 3, 5, 7, 9};
 		int[] v2 = {2, 4, 6, 8, 10};
 
-		int[] v3 = new int[10];
+		int[] v3 = new int[v1.length + v2.length];
 		
 		int c = 0;
 		
