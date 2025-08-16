b9ff4faa9178caa1d2e9274dff1f2949b68bbb50
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-16 08:41:43 -0300

    Update Exercicio12.java

--- src/estrutura/vetor/Exercicio12.java
+++ src/estrutura/vetor/Exercicio12.java
@@ -48,3 +48,6 @@ public class Exercicio12 {
 		
 	}
 }
+
+/*12 - Leia 10 números e armazene-os em um vetor. Solicite ao usuário que insira um valor a ser removido. 
+  Crie um novo vetor sem esse valor, caso ele exista.*/
