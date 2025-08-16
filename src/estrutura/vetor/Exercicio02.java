1685e9bf83d75854fdfb2defadd6b22ae870baf0
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-15 23:00:02 -0300

    Update Exercicio02.java

--- src/estrutura/vetor/Exercicio02.java
+++ src/estrutura/vetor/Exercicio02.java
@@ -9,15 +9,14 @@ public class Exercicio02 {
 		sc = new Scanner(System.in);
 		
 		int[] vetor = new int[5];
-		int numero, soma=0;
+		int  soma=0;
 		
 		System.out.println("Vamos Calcular!");
 		
 		for(int i = 0; i < vetor.length; i++) {
 			System.out.println("Numero " + (i+1) + ": ");
-			numero = sc.nextInt();			
-			vetor[i] = numero;
-			soma += numero;
+			vetor[i] = sc.nextInt();
+			soma += vetor[i];
 		}		
 		System.out.println("Soma Total = " + soma);
 
