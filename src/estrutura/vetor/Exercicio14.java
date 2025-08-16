33db4415d53cbddfc8b063bbfb0a30031b4c84c1
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-15 22:09:12 -0300

    Update Exercicio14.java

--- src/estrutura/vetor/Exercicio14.java
+++ src/estrutura/vetor/Exercicio14.java
@@ -1,17 +1,26 @@
 package estrutura.vetor;
 
+import java.util.Random;
+import java.util.Arrays;
+
 public class Exercicio14 {
-
 	public static void main(String[] args) {
-
+		Random aleatorio = new Random();
+		int[] numeros = new int[10];
+		
+		for(int i = 0; i < numeros.length; i++) {
+			numeros[i] = aleatorio.nextInt(11);
+		}
+		
+		int j = numeros.length - 1;
+		for(int i = 0; i <= 4; i++) {
+			if(j >= 5) {
+				int n = numeros[i];
+				numeros[i] = numeros[j];
+				numeros[j] = n;
+				j--;
+			}
+		}	
+		System.out.println("Vetor: " + Arrays.toString(numeros));
 	}
 }
-/*
-14 - Sortear 10 números e armazene-os em um vetor. 
-E inverter o vetor de forma que troque os elementos da:
-posição 0 com a posição 9
-posição 1 com a posição 8
-posição 2 com a posição 7
-posição 3 com a posição 6
-posição 4 com a posição 5
-*/
