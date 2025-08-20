3553d61b88cf41b99a1e0d9a6c91be1738365870
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-20 11:11:50 -0300

    Update Exemplo02.java

--- src/estrutura/vetor/Exemplo02.java
+++ src/estrutura/vetor/Exemplo02.java
@@ -3,7 +3,7 @@ package estrutura.vetor;
 public class Exemplo02 {
 	public static void main(String[] args) {
 		// declaração e inicialização do vetor
-		String[] lista = {"Ana", "Bia", "Dani", "Lu"};;
+		String[] lista = {"Ana", "Bia", "Dani", "Lu"};
 		
 		// usar o for each para imprimir todos os elementos da lista
 		for(String nome : lista) {
