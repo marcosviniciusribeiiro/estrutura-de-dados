c59d3ce656aa90348d39976e0add016a8944b5fc
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-22 10:56:24 -0300

    Create Exercicio07.java

---
+++ src/estrutura/matriz/Exercicio07.java
@@ -0,0 +1,9 @@
+package estrutura.matriz;
+
+public class Exercicio07 {
+	public static void main(String[] args) {
+		
+	}
+}
+/*7 - Crie um algoritmo que declare e atribua e depois calcule a soma dos valores da diagonal principal de uma matriz 5x5. 
+Ao final imprima a matriz e imprima o valor da variável soma*/
