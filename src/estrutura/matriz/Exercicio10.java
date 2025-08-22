4ec54b4faea42ab97056bc55641e829322eac152
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-22 13:14:22 -0300

    Update Exercicio10.java

--- src/estrutura/matriz/Exercicio10.java
+++ src/estrutura/matriz/Exercicio10.java
@@ -1,12 +1,44 @@
 package estrutura.matriz;
 
+import java.util.Random;
+
 public class Exercicio10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Random rd = new Random();
+		
+		int[][] matriz = new int [10][10];
+		
+		int maiorNum = 0;
+		
+		for(int i = 0; i < matriz.length; i++) {
+			for(int j = 0; j < matriz[i].length; j++) {
+				matriz[i][j] = rd.nextInt(100) + 1;
+				if(matriz[i][j] > maiorNum) {
+					maiorNum = matriz[i][j];
+				}
+			}
+		}
+		
+		System.out.println("Matriz");
+		
+		for(int[] linha: matriz) {
+			for(int elemento : linha) {
+				System.out.print(elemento + " ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("\nMaior Número: " + maiorNum);
+		
+		for(int i = 0; i < matriz.length; i++) {
+			for(int j = 0; j < matriz[i].length; j++) {
+				if(matriz[i][j] == maiorNum) {
+					System.out.println("\nPosição do Maior Número:\nLinha " + i + "\nColuna " + j);
+				}
+			}
+		}
 	}
-
 }
 /*10 – Declare e atribua com números sorteados de 1 ATE 100 uma matriz 10x10 
  * ao final imprimir toda a matriz e o maior numero dentro da matriz e informe as posições (linha e coluna) 
