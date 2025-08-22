505ab5349774c1d5749732f508a4a040f58eca7e
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-22 10:59:26 -0300

    Create Exercicio09.java

---
+++ src/estrutura/matriz/Exercicio09.java
@@ -0,0 +1,7 @@
+package estrutura.matriz;
+
+public class Exercicio09 {
+
+}
+/*9 – Criar uma matriz nota de 5x4 e atribua notas para a matriz declarada.
+Armazene a media de cada linha em um vetor media[5] ao final imprima a matriz e o vetor*/
