304217f30f0626ea24a20cb173c47af682aae520
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-22 10:56:26 -0300

    Create Exercicio08.java

---
+++ src/estrutura/matriz/Exercicio08.java
@@ -0,0 +1,9 @@
+package estrutura.matriz;
+
+public class Exercicio08 {
+	public static void main(String[] args) {
+		
+	}
+}
+/*8 - Crie um algoritmo que declare e inicialize uma matriz 5x5 com números sorteados de 1 ate 10, sendo que todos os elementos abaixo da diagonal principal deverá ser igual a 0.
+Ao final imprima a matriz completa*/
