1d62a53a254767aa4e2fe6ae8401b5febb85bfed
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-22 10:59:31 -0300

    Create Exercicio12.java

---
+++ src/estrutura/matriz/Exercicio12.java
@@ -0,0 +1,11 @@
+package estrutura.matriz;
+
+public class Exercicio12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+/*12 – Declare uma matriz de 10x10 e atribua a cada posição da matriz a soma das posições de linha e coluna. Ao final imprima toda a matriz.*/
