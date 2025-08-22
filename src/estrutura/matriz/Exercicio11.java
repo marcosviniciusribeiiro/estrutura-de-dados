59d4559587e6ba22468f41fdc637f5b33c965f93
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-22 10:59:30 -0300

    Create Exercicio11.java

---
+++ src/estrutura/matriz/Exercicio11.java
@@ -0,0 +1,13 @@
+package estrutura.matriz;
+
+public class Exercicio11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+/*11 – Declare e atribua com números sorteados de 1 ATE 100 uma matriz 10x10.
+ * Ao final leia um numero e procure o numero dentro da matriz e informe qual a posição linha e coluna que o numero foi encontrado. 
+ * Caso o numero não exista na matriz exiba uma mensagem que o numero não existe na matriz.*/
