1df510670232b8bf5191b5762fbc72cb1bb60de8
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-22 11:34:51 -0300

    Update Exercicio06.java

--- src/estrutura/matriz/Exercicio06.java
+++ src/estrutura/matriz/Exercicio06.java
@@ -8,12 +8,26 @@ public class Exercicio06 {
 		
 		int[][] m1 = new int[2][5];
 		int[][] m2 = new int[2][5];
+		int[][] m3 = new int[2][5];
 		
 		for(int i = 0; i < 2; i++) {
 			for(int j = 0; j < 5; j++) {
 				m1[i][j] = rd.nextInt(10) + 1;
 				m2[i][j] = rd.nextInt(10) + 1;
 			}
+		}
+		
+		for(int i = 0; i < 2; i++) {
+			for(int j = 0; j < 5; j++) {
+				m3[i][j] = m1[i][j] + m2[i][j];
+			}
+		}
+		
+		for(int[] linha: m3) {
+			for(int elemento : linha) {
+				System.out.print(elemento + " ");			
+			}
+			System.out.println();
 		}
 		
 	}
