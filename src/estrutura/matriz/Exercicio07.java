a21230208ecedfcf9ea0aa582a0df97b7b6f523f
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-22 12:33:57 -0300

    Update Exercicio07.java

--- src/estrutura/matriz/Exercicio07.java
+++ src/estrutura/matriz/Exercicio07.java
@@ -2,7 +2,22 @@ package estrutura.matriz;
 
 public class Exercicio07 {
 	public static void main(String[] args) {
+		int[][] mat = {{1,2,3}, {4,5,6}, {7,8,9}};
+		int soma = 0;
 		
+		System.out.println("Matriz");
+		
+		for(int i = 0; i < mat.length; i++) {
+			for(int j = 0; j < mat[i].length; j++) {
+				System.out.print(mat[i][j] + " ");
+				if(i == j) {
+					soma += mat[i][j];
+				}
+			}
+			System.out.println();
+		}
+		
+		System.out.println("\nSoma dos Valores da Diagonal Principal = " + soma);
 	}
 }
 /*7 - Crie um algoritmo que declare e atribua e depois calcule a soma dos valores da diagonal principal de uma matriz 5x5. 
