bdf96312542a7e8144ddf627819b259ad45088c0
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-22 10:59:28 -0300

    Create Exercicio10.java

---
+++ src/estrutura/matriz/Exercicio10.java
@@ -0,0 +1,13 @@
+package estrutura.matriz;
+
+public class Exercicio10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+/*10 – Declare e atribua com números sorteados de 1 ATE 100 uma matriz 10x10 
+ * ao final imprimir toda a matriz e o maior numero dentro da matriz e informe as posições (linha e coluna) 
+ * que o maior numero está armazenado.*/
