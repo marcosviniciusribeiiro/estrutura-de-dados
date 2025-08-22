d4154b9e210927f29194e56eab190d924feee455
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-08-22 11:34:45 -0300

    Update Exercicio05.java

--- src/estrutura/matriz/Exercicio05.java
+++ src/estrutura/matriz/Exercicio05.java
@@ -2,20 +2,20 @@ package estrutura.matriz;
 
 public class Exercicio05 {
 	public static void main(String[] args) {
-		int[][] matriz = {{1,2,3},
+		int[][] mat = {{1,2,3},
 		                  {4,5,6},
 		                  {7,8,9}};
 		
-		int[][]matrizInversa = new int [3][3];
+		int[][]matInversa = new int [3][3];
 		
 		for(int i = 0; i < 3; i++) {
 			for(int j = 0; j < 3; j++) {
-				matrizInversa[i][j] = matriz[j][i];
+				matInversa[i][j] = mat[j][i];
 			}
 		}
 		
 		
-		for(int[] linha : matrizInversa) {
+		for(int[] linha : matInversa) {
 			for(int elemento: linha) {
 				System.out.print(elemento + " ");
 			}
