afbf2bc3bdc0350c62fac2379cd1e6bc1527e89d
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-09-05 10:52:57 -0300

    Update TestarFilaAtendimento.java

--- src/estrutura/fila/exercicio/TestarFilaAtendimento.java
+++ src/estrutura/fila/exercicio/TestarFilaAtendimento.java
@@ -3,6 +3,11 @@ package estrutura.fila.exercicio;
 public class TestarFilaAtendimento {
 	public static void main(String[] args) {
 		FilaAtendimento<Cliente> fila = new FilaAtendimento<>();
-		fila.adicionarCliente("Marcos");
+		fila.adicionarCliente(new Cliente("Marcos", FormaDePagamento.Pix));
+		fila.adicionarCliente(new Cliente("Ana", FormaDePagamento.Dinheiro));
+		
+
+		System.out.println("Atendendo cliente: " + fila.atenderCliente());
+		System.out.println("Atendendo cliente: " + fila.atenderCliente());
 	}	
 }
