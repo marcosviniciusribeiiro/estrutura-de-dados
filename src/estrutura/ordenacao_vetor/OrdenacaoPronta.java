336560b6b0d693f6ca004c9654f52a7f87be7e2f
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-03 10:49:17 -0300

    Update OrdenacaoPronta.java

--- src/estrutura/ordenacao_vetor/OrdenacaoPronta.java
+++ src/estrutura/ordenacao_vetor/OrdenacaoPronta.java
@@ -5,8 +5,11 @@ import java.util.Arrays;
 public class OrdenacaoPronta {
 	public static void main(String[] args) {
 		int[] vetor = {22,11,6, 66, 29, 5, 7};
+		String[] nomes = {"João", "Carlos", "Ana", "Pedro", "Julia"};
 		System.out.println("Vetor: " + Arrays.toString(vetor));
 		Arrays.sort(vetor);
-		System.out.println("Vetor ordenado: "+ Arrays.toString(vetor));
+		Arrays.sort(nomes);
+		System.out.println("Numeros ordenados: "+ Arrays.toString(vetor));
+		System.out.println("Nomes ordenados: "+ Arrays.toString(nomes));
 	}
 }
