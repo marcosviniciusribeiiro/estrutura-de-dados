a026968bbebded5d657349ea08ecfeacdee20349
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-24 10:22:10 -0300

    Create No.java

---
+++ src/estrutura/arvore/No.java
@@ -0,0 +1,13 @@
+package estrutura.arvore;
+
+public class No {
+	public int valor;
+	public No esquerda;
+	public No direita;
+	
+	public No (int valor) {
+		this.valor = valor;
+		this.esquerda = null;
+		this.direita = null;
+	}
+}
