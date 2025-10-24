04f5014d5c6998a59d782b39e47e3c4bb8af1bcc
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-24 09:52:32 -0300

    Create FatorialFor.java

---
+++ src/estrutura/funcao/recursiva/FatorialFor.java
@@ -0,0 +1,21 @@
+package estrutura.funcao.recursiva;
+
+public class FatorialFor {
+	
+	public static void main(String[] args) {
+		int num = 5;
+		
+		int fatorial = calcularFatorial(num);
+		
+		System.out.println(num + "! = " + fatorial);
+	}
+	
+	// fatorial sem utilizar a função recursiva
+	public static int calcularFatorial(int num) {
+		int resultado = 1;
+		for(int i = num; i > 0; i--) {
+			resultado *= i;
+		}
+		return resultado;
+	}
+}
