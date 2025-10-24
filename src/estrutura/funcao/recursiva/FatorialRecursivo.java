f8b4020fd4dfeb821ecedcfd70e57ef70dd1fb5b
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-24 09:58:26 -0300

    Update FatorialRecursivo.java

--- src/estrutura/funcao/recursiva/FatorialRecursivo.java
+++ src/estrutura/funcao/recursiva/FatorialRecursivo.java
@@ -3,7 +3,7 @@ package estrutura.funcao.recursiva;
 public class FatorialRecursivo {
 
 	public static void main(String[] args) {
-		int num = 3;
+		int num = 4;
 		
 		int fatorial = fatorial(num);
 		
@@ -12,9 +12,13 @@ public class FatorialRecursivo {
 	
 	// fatorial utilizando a função recursiva	
 	public static int fatorial(int num) {
-		if(num == 0 | num == 1) {
+		if(num == 0 || num == 1) {
 			return 1;
 		}
 		return num *= fatorial(num-1);
 	}
 }
+// 4! = 4 * 3 * 2 * 1
+// 3! = 3 * 2 * 1
+// 2! = 2 * 1
+// 1! = 1
