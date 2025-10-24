074d7e46c3126a082acac610233d0f65f64f7888
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-24 09:52:52 -0300

    Create FatorialRecursivo.java

---
+++ src/estrutura/funcao/recursiva/FatorialRecursivo.java
@@ -0,0 +1,20 @@
+package estrutura.funcao.recursiva;
+
+public class FatorialRecursivo {
+
+	public static void main(String[] args) {
+		int num = 0;
+		
+		int fatorial = fatorial(num);
+		
+		System.out.println(num + "! = " + fatorial);
+	}
+	
+	// fatorial utilizando a função recursiva	
+	public static int fatorial(int num) {
+		if(num == 0 | num == 1) {
+			return 1;
+		}
+		return num *= fatorial(num-1);
+	}
+}
