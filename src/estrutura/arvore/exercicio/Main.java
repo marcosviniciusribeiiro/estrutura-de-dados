d67120010f584a3a089de65a4d29ffd0e88df700
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-31 10:41:20 -0300

    Update Main.java

--- src/estrutura/arvore/exercicio/Main.java
+++ src/estrutura/arvore/exercicio/Main.java
@@ -4,5 +4,23 @@ public class Main {
 	public static void main(String[] args) {
 		ArvoreBinaria arvore = new ArvoreBinaria();
 		
+		// inserindo as palavras
+		arvore.inserir("Banana");
+		arvore.inserir("Maçã");
+		arvore.inserir("Laranja");
+		arvore.inserir("Abacate");
+		arvore.inserir("Abacaxi");
+		arvore.inserir("Uva");
+		
+		// impressão em ordem
+		arvore.imprimirEmOrdem();
+		
+		// buscando palavras
+		String busca1 = "uva";
+		String busca2 = "melão";
+		
+		System.out.println("\nBusca por \"" + busca1 + "\": " + (arvore.buscar(busca1) ? "Encontrada" : "Não encontrada"));
+		
+		System.out.println("\nBusca por \"" + busca2 + "\": " + (arvore.buscar(busca2) ? "Encontrada" : "Não encontrada"));
 	}
 }
