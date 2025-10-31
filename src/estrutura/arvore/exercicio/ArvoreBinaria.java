fb03b5431ab3ba70ab2475905b622ebe3a951601
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-31 10:12:14 -0300

    Update ArvoreBinaria.java

--- src/estrutura/arvore/exercicio/ArvoreBinaria.java
+++ src/estrutura/arvore/exercicio/ArvoreBinaria.java
@@ -1,9 +1,36 @@
 package estrutura.arvore.exercicio;
 
 public class ArvoreBinaria {
-	No raiz;
+	private No raiz;
 	
 	public ArvoreBinaria() {
-		raiz = null;
+		this.raiz = null;
 	}
+	
+	// Inserir palavra na arvore binária
+	public void inserir(String palavra) {
+		raiz = inserirRec(raiz, palavra.toLowerCase());
+	}
+	
+	private No inserirRec(No atual, String palavra) {
+		if(atual == null) {
+			return new No(palavra);
+		}
+		int comparacao = palavra.compareTo(atual.palavra);
+		
+		if(comparacao < 0) {
+			atual.esquerda = inserirRec(atual.esquerda, palavra);
+		} else if (comparacao > 0) {
+			atual.direita = inserirRec(atual.direita, palavra);
+		} else {
+			// Não insere duplicatas
+			System.out.println("A palavra \"" + palavra + "\" já existe na árvore binária.");
+		}
+		
+		return atual;
+	}
+	
+	// Buscar por uma palavra
+	
+	// Imprimir em ordem alfabética
 }
