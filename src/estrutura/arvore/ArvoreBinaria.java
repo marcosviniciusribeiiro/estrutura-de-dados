0319a2d62b1a8d21c0e311fcfa916825371cde2e
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-10-31 09:10:21 -0300

    Update ArvoreBinaria.java

--- src/estrutura/arvore/ArvoreBinaria.java
+++ src/estrutura/arvore/ArvoreBinaria.java
@@ -4,7 +4,7 @@ public class ArvoreBinaria {
 	No raiz;
 	
 	public ArvoreBinaria () {
-		this.raiz = null;
+		raiz = null;
 	}
 	
 	// Inserção de um Nó com função recursiva
@@ -20,5 +20,25 @@ public class ArvoreBinaria {
 			raiz.direita = inserir(raiz.direita, valor);
 		}
 		return raiz;
+	}
+	
+	// Busca Recursiva
+	public boolean buscar(No raiz, int valor) {
+		if(raiz == null) 
+			return false;
+		
+		if(raiz.valor == valor)
+			return true;
+		
+		return valor < raiz.valor ? buscar(raiz.esquerda, valor) : buscar(raiz.direita, valor);
+	}
+	
+	// Percurso em Ordem
+	public void emOrdem(No raiz) {
+		if(raiz != null) {
+			emOrdem(raiz.esquerda);
+			System.out.print(raiz.valor + " ");
+			emOrdem(raiz.direita);
+		}
 	}
 }
