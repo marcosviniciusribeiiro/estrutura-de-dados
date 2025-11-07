2c3b957a58dad1bc24bd6774790add1bb51a9096
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-11-07 08:42:31 -0300

    Create PesquisaBinaria.java

---
+++ src/estrutura/pesquisa_binaria/PesquisaBinaria.java
@@ -0,0 +1,24 @@
+package estrutura.pesquisa_binaria;
+
+public class PesquisaBinaria {
+	public static int buscaBinaria(int[] vetor, int numProc) {
+		int inicio = 0;
+		int fim = vetor.length - 1;
+		int meio;
+		
+		while(inicio <= fim) {
+			meio = (inicio + fim) / 2;
+			
+			if(vetor[meio] == numProc) {
+				return meio;
+			} else if (vetor[meio] < numProc) {
+				inicio = meio + 1;
+			} else {
+				fim = meio - 1;
+			}
+			
+		}
+		
+		return -1;
+	}
+}
