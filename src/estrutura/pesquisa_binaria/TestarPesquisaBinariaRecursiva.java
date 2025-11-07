70e3544f7435774cdb33457e58d3a9fc209a2708
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-11-07 09:39:49 -0300

    Create TestarPesquisaBinariaRecursiva.java

---
+++ src/estrutura/pesquisa_binaria/TestarPesquisaBinariaRecursiva.java
@@ -0,0 +1,16 @@
+package estrutura.pesquisa_binaria;
+
+public class TestarPesquisaBinariaRecursiva {
+	public static void main(String[] args) {
+		int[] vetor = {1, 3,4,9,10, 12, 14,18,19, 22, 25,30, 32, 35};
+		int numProcurado = 22;
+		
+		int resultado = PesquisaBinariaRecursiva.buscaBinaria(vetor, numProcurado, 0, vetor.length-1);
+		
+		if(resultado == -1) {
+			System.out.println("Não encontrado!");
+		} else {
+			System.out.println("Número " + numProcurado + " foi encontrado na posiçao: " + resultado);
+		}
+	}
+}
